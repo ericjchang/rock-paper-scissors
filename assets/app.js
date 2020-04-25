3bae8d3c27d3bca69a1652a0f85239b2ecdf8455
Eric J. Chang <39933004+ericjchang@users.noreply.github.com>
2020-04-26 04:47:05 +0700

    Revert "v1.0.1"

--- assets/app.js
+++ assets/app.js
@@ -15,7 +15,6 @@ function getComputerChoice() {
   return choices[randonNumber];
 }
 
-// convert letter to word
 function convertToWord(letter) {
   switch (letter) {
     case "r":
@@ -30,43 +29,14 @@ function convertToWord(letter) {
   }
 }
 
-// change score in score-board
 function changeScore() {
   userScore_span.innerHTML = userScore;
   compScore_span.innerHTML = compScore;
 }
 
-// change picture of player choice in score-board
-function changePicture(letterUser, letterComp) {
-  switch (letterUser) {
-    case "r":
-      document.getElementById("user-choice").innerHTML = '<img src="assets/images/rock.png" alt="rock" />';
-      break;
-    case "p":
-      document.getElementById("user-choice").innerHTML = '<img src="assets/images/paper.png" alt="paper" />';
-      break;
-    case "s":
-      document.getElementById("user-choice").innerHTML = '<img src="assets/images/scissors.png" alt="rock" />';
-      break;
-  }
-
-  switch (letterComp) {
-    case "r":
-      document.getElementById("comp-choice").innerHTML = '<img src="assets/images/rock.png" alt="rock" />';
-      break;
-    case "p":
-      document.getElementById("comp-choice").innerHTML = '<img src="assets/images/paper.png" alt="paper" />';
-      break;
-    case "s":
-      document.getElementById("comp-choice").innerHTML = '<img src="assets/images/scissors.png" alt="rock" />';
-      break;
-  }
-}
-
 function gameResult(userChoice, compChoice, result) {
   const userLetter = "user".fontsize(3).sub();
   const compLetter = "comp".fontsize(3).sub();
-  changePicture(userChoice, compChoice);
   switch (result) {
     case "win":
       userScore++;
@@ -74,7 +44,6 @@ function gameResult(userChoice, compChoice, result) {
       result_div.innerHTML = `${convertToWord(userChoice)}${userLetter} beats ${convertToWord(
         compChoice
       )}${compLetter} You win!"`;
-      document.getElementById("action-message").innerHTML = "Easy peasy right ?";
       document.getElementById(userChoice).classList.add("green-glow");
       setTimeout(() => {
         document.getElementById(userChoice).classList.remove("green-glow");
@@ -86,7 +55,6 @@ function gameResult(userChoice, compChoice, result) {
       result_div.innerHTML = `${convertToWord(userChoice)}${userLetter} beats ${convertToWord(
         compChoice
       )}${compLetter} You lose!"`;
-      document.getElementById("action-message").innerHTML = "Oh, Come on";
       document.getElementById(userChoice).classList.add("red-glow");
       setTimeout(() => {
         document.getElementById(userChoice).classList.remove("red-glow");
@@ -96,16 +64,12 @@ function gameResult(userChoice, compChoice, result) {
       result_div.innerHTML = `${convertToWord(userChoice)}${userLetter} equals ${convertToWord(
         compChoice
       )}${compLetter} Draw!"`;
-      document.getElementById("action-message").innerHTML = "Try again !!! Come on...";
       document.getElementById(userChoice).classList.add("grey-glow");
       setTimeout(() => {
         document.getElementById(userChoice).classList.remove("grey-glow");
       }, 500);
       break;
   }
-  setTimeout(() => {
-    document.getElementById("action-message").innerHTML = "Make your move";
-  }, 3000);
 }
 
 function game(userChoice) {
