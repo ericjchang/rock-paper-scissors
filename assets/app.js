547fa75bc135803915d7724db9daef5ef86d384a
ericjchang <eric.jaspchang@gmail.com>
2020-04-26 05:15:43 +0700

    v1.0.1

--- assets/app.js
+++ assets/app.js
@@ -15,6 +15,7 @@ function getComputerChoice() {
   return choices[randonNumber];
 }
 
+// convert letter to word
 function convertToWord(letter) {
   switch (letter) {
     case "r":
@@ -29,14 +30,43 @@ function convertToWord(letter) {
   }
 }
 
+// change score in score-board
 function changeScore() {
   userScore_span.innerHTML = userScore;
   compScore_span.innerHTML = compScore;
 }
 
+// change picture of player choice in score-board
+function changePicture(letterUser, letterComp) {
+  switch (letterUser) {
+    case "r":
+      document.getElementById("user-choice").innerHTML = '<img src="assets/images/rock.png" alt="rock" />';
+      break;
+    case "p":
+      document.getElementById("user-choice").innerHTML = '<img src="assets/images/paper.png" alt="paper" />';
+      break;
+    case "s":
+      document.getElementById("user-choice").innerHTML = '<img src="assets/images/scissors.png" alt="rock" />';
+      break;
+  }
+
+  switch (letterComp) {
+    case "r":
+      document.getElementById("comp-choice").innerHTML = '<img src="assets/images/rock.png" alt="rock" />';
+      break;
+    case "p":
+      document.getElementById("comp-choice").innerHTML = '<img src="assets/images/paper.png" alt="paper" />';
+      break;
+    case "s":
+      document.getElementById("comp-choice").innerHTML = '<img src="assets/images/scissors.png" alt="rock" />';
+      break;
+  }
+}
+
 function gameResult(userChoice, compChoice, result) {
   const userLetter = "user".fontsize(3).sub();
   const compLetter = "comp".fontsize(3).sub();
+  changePicture(userChoice, compChoice);
   switch (result) {
     case "win":
       userScore++;
@@ -44,6 +74,7 @@ function gameResult(userChoice, compChoice, result) {
       result_div.innerHTML = `${convertToWord(userChoice)}${userLetter} beats ${convertToWord(
         compChoice
       )}${compLetter} You win!"`;
+      document.getElementById("action-message").innerHTML = "Easy peasy right ?";
       document.getElementById(userChoice).classList.add("green-glow");
       setTimeout(() => {
         document.getElementById(userChoice).classList.remove("green-glow");
@@ -55,6 +86,7 @@ function gameResult(userChoice, compChoice, result) {
       result_div.innerHTML = `${convertToWord(userChoice)}${userLetter} beats ${convertToWord(
         compChoice
       )}${compLetter} You lose!"`;
+      document.getElementById("action-message").innerHTML = "Oh, Come on";
       document.getElementById(userChoice).classList.add("red-glow");
       setTimeout(() => {
         document.getElementById(userChoice).classList.remove("red-glow");
@@ -64,6 +96,7 @@ function gameResult(userChoice, compChoice, result) {
       result_div.innerHTML = `${convertToWord(userChoice)}${userLetter} equals ${convertToWord(
         compChoice
       )}${compLetter} Draw!"`;
+      document.getElementById("action-message").innerHTML = "Try again !!! Come on...";
       document.getElementById(userChoice).classList.add("grey-glow");
       setTimeout(() => {
         document.getElementById(userChoice).classList.remove("grey-glow");
